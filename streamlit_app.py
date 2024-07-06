61bc0779290c1d5bd8f139183c2c4cdd8d565010
Abida Adra <abidaadra24@gmail.com>
2024-07-06 15:54:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#title
+st.title("🎈 My first app")
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
 
+#writing this
 st.write("If you see this it works!!!")
+
+#Header
+st.header("Header") 
+
+#Subheader
+st.subheader("Subheader")
