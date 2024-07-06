284f3c84d15f99b2463e80aaf2ca926c828e8003
Abida Adra <abidaadra24@gmail.com>
2024-07-06 16:01:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,46 @@
 import streamlit as st
 
 #title
-st.title("🎈 My first app")
+st.title("🎈 My first app - This is the title.")
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+st.write("For more info about streamlit functions go to: https://github.com/abidaadra/my_firststreamlit_app")
 
-#writing this
-st.write("If you see this it works!!!")
+#Write Text
+st.write("Write")
 
 #Header
 st.header("Header") 
 
 #Subheader
 st.subheader("Subheader")
+
+#Text
+st.text("Text")
+
+#Markdown
+st.markdown("### Markdown")
+
+#Success
+st.success("Success")
+
+#Information
+st.info("Information")
+
+#Warning
+st.warning("Warning")
+
+#Error
+st.error("Error")
+
+# Exception
+exp = ZeroDivisionError("Exception when trying to divide by Zero")
+st.exception(exp)
+
+#Built-in Range function
+st.write(range(10))
+
+
+
+
+
+
