d497aea09ea2c279a8940b741f9b48b5f76faedd
Abida Adra <abidaadra24@gmail.com>
2024-07-06 16:29:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,6 +67,36 @@ if (status == 'Male'):
 else:
 	st.success("Female")
 
+#Selection box
+#1st argument takes the titleof the selectionbox
+#2nd argument takes options
+hobby = st.selectbox("Hobbies: ",['Dancing', 'Reading', 'Sports'])
+
+#Print the selected hobby
+st.write("Your hobby is: ", hobby)
+
+# Create a simple button that does nothing
+st.button("Click me which does nothing")
+
+# Create a button, that when clicked, shows a text
+if(st.button("About")):
+	st.text("My name is Abida Adra")
+
+#Text Input
+#Save the input text in the variable 'name'
+#1st argument shows the title of the text input box
+#2nd argument displays a default text inside the text input area
+name = st.text_input("Enter Your name", "Type Here ...")
+
+#Display the name when the submit button is clicked
+#.title() is used to get the input text string
+if(st.button('Submit')):
+	result = name.title()
+	st.success(result)
+
+
+
+
 
 
 
